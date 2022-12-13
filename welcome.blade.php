dcdbd1da28ab20cfac1cb56eec6b3ea02fb099a8
BlueCoder's <85623180+kaneibra@users.noreply.github.com>
2022-12-13 00:16:54 +0000

    ajout initial de fichiers

---
+++ welcome.blade.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<!-- Link CSS3 -->
+	<link rel="stylesheet" href="css/main.css">
+	<!-- End Link CSS3 -->
+	
+	<!-- Script Js -->
+<script src="js/toggle-mode.js"></script>
+
+	<!-- End Script Js -->
+
+
+	<title>WEBSITE ASSAFE</title>
+</head>
+<body>
+	<!-- infos_social_network_header -->
+	<div class="infos-ong-social-network">
+		<div class="logo_social_network">
+			<img src="img/logo/fcblogo.png" alt="" class="fcb-logo" id="fcb-logo">
+			<img src="img/logo/twit.png" alt="" class="twitter-logo" id="twitter-logo">
+			<img src="img/logo/carmika-partners-llp-is-authorised-and-regulated-by-logo-linkedin-blanc-11562891837uivlzbtyts.jpeg" alt="" class="linkedin-logo" id="linkedin-logo">
+		</div>
+	</div>
+	
+	<!-- End infos_social_network_header -->
+
+	<!-- Head Infos -->
+	<div class="head_infos">
+	<!--Logo de l'ONG -->
+	<img src="" alt="" class="logo_ong" id="logo_ong">
+	<!--End Logo de l'ONG -->
+	
+	<div class="mail_infos">
+		<div class="bg_icon_msg"></div>
+		<img src="img/logo/message.jpeg" alt="" class="t_icon_msg_img" id="icon_msg_img">
+			<h2 class="text_mail">Mail</h2>
+			<h2 class="text_mail_assafe">info@assafe.org</h2>
+		
+		</div>
+	<div class="call_infos">
+
+		<div class="bg_icon_call"></div>
+		<img src="img/logo/icon_call.png" alt="" class="icon_call" id="icon_call">
+		<h2 class="call_us">Call Us</h2>
+		<h2 class="call_number">+22382258505</h2>
+	</div>
+	
+	</div>
+	 
+	<!-- End Head Infos -->
+	<!-- Nav_Bar_Menu Static   -->
+	<div class="nav_bar_menu_static">
+		<ul class="nav_menu">
+			<li class="link_home"><a href="#" class="home" id="home">HOME</a></li>
+			<li class="link_about"><a href="#" class="about" id="about">ABOUT</a> <span class="arrow"> > </span> </li>
+			<li class="link_causes"><a href="#" class="cause" id="cause">CAUSES</a> <span class="arrow_cause"> > </span></li>
+			<li class="link_pages"><a href="#" class="pages" id="pages">PAGES</a> <span class="arrow_page"> > </span></li>
+			<li class="link_events"><a href="#" class="events" id="events">EVENTS</a> </li>
+			<li class="link_contact_us"><a href="#" class="contact" id="contact">CONTACT US</a> </li>
+			
+		</ul>
+
+		<!-- <button class="btn-toggle-theme" onclick="MyFunction()">dark</button> -->
+	</div>
+	<!-- End  Nav_Bar_Menu Static -->
+
+	<!-- bg_header  -->
+
+ <div class="headline">
+	 	<div class="p_motivation">
+			<div class="border_motiv">	 
+			</div>
+			<div class="against_violence">
+				<h3 style="color: #FF7D05;" class="p_motiv_color_orange">#EndViolence
+			
+			</h3>
+				<h3 style="color: #FFFFFF;" class="p_motiv_or_2"> For <br><span class="p_mo2_2">Every Woman</span> </h3>
+			</div>
+	 	</div>
+		 <div class="p_sub_border"></div>
+		 <h4 class="sub_motivation">lorem ipsuñ khalil va la nertum parlem de la capita</h4>
+ </div>
+ 
+ <!-- End bg_header  -->
+ <!-- Motivation Section   -->
+ <div class="motivation_section">
+	 <h3 class="engengement">
+	 We are <span style="color: #FF7D05;">volunteers</span> and you ?
+	 </h3>
+	 <hr class="underline">
+
+ </div>
+ <div class="motivation_paragraph">
+ lorem ipsuñ khalil va la nertum parlem de la capita lorem ipsuñ khalil va la nertum parlem de la capita
+ </div>
+ <!-- End  Motivation Section -->
+ 
+ <!-- Card Section pub -->
+ 
+ <div class="card_section">
+	 <div class="card-box">
+
+	 <!-- Card One -->
+	 <div class="card_one">
+		 <img src="img/card_image/téléchargement.jpeg" alt="" srcset="" class="img_card_one">
+		 <div class="card_title">
+			 EDUCATION FOR CINZANI CHILD
+			</div>
+			<div class="card_sub_title">
+				specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.remaining essentially unchanged.
+				It was popularised in the 1960s with the 
+				release of Letraset sheets
+			</div>
+			<hr class="underline_card">
+			<div class="city">
+				<img src="img/card_image/localisationlocalisation.png" alt="" srcset="" class="localisation-city">
+				<span class="city-name">BAMAKO</span>
+			</div>
+			<div class="card-post_time">
+				<img src="img/card_image/watch_icon.png" alt="" srcset="" class="watch_icon">
+				<span class="time-text">
+					12:00
+				</span>
+			</div>
+		</div>
+		<!-- Card Two -->
+		<div class="card_two">
+			<img src="img/card_image/wo.jpeg" alt="" srcset="" class="img_card_two">
+			<div class="card_title_2">
+				WOMAN SENSIBILISATION			</div>
+				<div class="card_sub_title_2">
+					specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.remaining essentially unchanged.
+					It was popularised in the 1960s with the 
+					release of Letraset sheets
+				</div>
+				<hr class="underline_card_2">
+				<div class="city_2">
+					<img src="img/card_image/localisationlocalisation.png" alt="" srcset="" class="localisation-city_2">
+					<span class="city-name_2">CINZANA</span>
+				</div>
+				<div class="card-post_time_2">
+					<img src="img/card_image/watch_icon.png" alt="" srcset="" class="watch_icon_2">
+					<span class="time-text_2">
+						9:00
+					</span>
+				</div>
+			</div>
+			<!-- End Card Two -->
+			
+		<!-- Card THREE -->
+		<div class="card_three">
+			<img src="img/card_image/wo2.webp" alt="" srcset="" class="img-car-3">
+			<div class="card-title-3">
+			KIT AGRICULTURE  OFFERT
+			</div>
+			<div class="card_sub_title-3">
+			specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.remaining essentially unchanged.
+ 			It was popularised in the 1960s with the 
+			release of Letraset sheets
+			</div>
+			<hr class="underline_card_3">
+			<div class="city_3">
+					<img src="img/card_image/localisationlocalisation.png" alt="" srcset="" class="localisation-city_3">
+					<span class="city-name_3">SOROFING</span>
+				</div>
+				<div class="card-post_time_3">
+					<img src="img/card_image/watch_icon.png" alt="" srcset="" class="watch_icon_3">
+					<span class="time-text_3">
+						9:00
+					</span>
+				</div>
+			</div>
+			<!-- End   Card THREE -->
+
+			<!-- Button More View -->
+			
+			<button class="btn-more_cause">
+			<a href="#" style="color: #FFFFFF;" class="see_all_cause" id="see_all_cause">SEE ALL CAUSE</a>
+			</button>
+
+			<!-- End  Button More View -->
+	</div>
+</div>
+
+<!-- End   Card Section pub-->
+<!-- main_causes_section   -->
+
+<div class="main_causes_section">
+	<div class="bg_cause"></div>
+
+	<!-- Youtube channel testimonials -->
+	<img src="img/card_image/people-gc8d44c2d7_1920.jpg"  alt=" " class="youtube_channel">
+	<!-- End Youtube channel testimonials -->
+	<!-- text testimonial youtube -->
+	<p class="text_testimonial">
+		<span style="color: #FF7D05;">#Témoignages</span> de femmes et jeunes filles satisfaitent de la sensibilisation et de la formation  des agents de <span style="color: #FF7D05;">L’ONG ASSAFE</span>
+	</p>
+	<!-- End text testimonial youtube -->
+
+	<!-- Btn YOUTUBE CHANNEL  -->
+	<button type="submit" class="button_subscribe">
+		<span id="btn_text_subscribe">SUBSCRIBE</span>
+	</button>
+	<div class="btn_watch">
+		<p class="WATCH">
+		WATCH
+		</p>
+		<img src="img/card_image/yout.png" alt="" class="watch_iconify" id="watch_iconify">
+	</div>
+	<!-- End Btn YOUTUBE CHANNEL  -->
+</div>
+<!-- End main_causes_section  -->
+
+<!-- About Us Section -->
+
+<div class="about_us">
+	<img src="img/card_image/w.jpeg" alt="" srcset="" class="img_man">
+	<h2 class="contact_follow">How To <span style="color: #FF7D05;">Contact</span> US</h2>
+	<p class="contact_follow_paragraph">
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+	</p>
+	<div class="contact_instruction">
+		<div class="contact_1">
+			<div class="Ellipse_1"></div>
+			<h3 class="n1">1</h3>
+			<h2 class="Social_Network">Social Network</h2>
+			<p class="Social_Network_paragraph">
+			Lorem Ipsum is simply dummy text ovf the printing and 
+			typesetting industry. Lorem Ipsum has been the industry's 
+			standard
+			</p>
+		</div>
+		<div class="contact_2">
+			<div class="Ellipse_2"></div>
+			<h3 class="n2">2</h3>
+			<h2 class="Mail_and_Celular"> Mail and Celular</h2>
+			<p class="Mail_and_Celular_paragraph">
+			Lorem Ipsum is simply dummy text ovf the printing and 
+			typesetting industry. Lorem Ipsum has been the industry's 
+			standard
+			</p>
+		</div>
+		<div class="contact_3">
+			<div class="Ellipse_3"></div>
+			<h3 class="n3">3</h3>
+			<h2 class="localisation"> localisation</h2>
+			<p class="localisation_paragraph">
+			Lorem Ipsum is simply dummy text ovf the printing and 
+			typesetting industry. Lorem Ipsum has been the industry's 
+			standard
+			</p>
+		</div>
+	</div>
+</div>
+<!-- End About Us -->
+
+<!-- Explain Motivation Section -->
+<div class="motiv_section_explain">
+	<div class="section_motiv_expl_2">
+		<h1 class="title_section_motiv_expl_2">
+		We Help Many <span style="color: #FF7D05;">People</span>
+		</h1>
+		<p class="text_section_motiv_expl_2">
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+
+		</p>
+	</div>
+</div>
+<!-- End Explain Motivation Section -->
+<!-- section_gallery -->
+<div class="opacity_gallery">
+</div>
+<div class="section_gallery">
+	<h1 class="title_gallery">Our <span style="color: #FF7D05;">Gallery</span></h1>
+	<p class="sub_title_gallery">
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 
+	</p>
+	<!-- card image section  -->
+	<div class="gallery_card">
+<img src="img/gallery/girl-gd6efac215_1920.jpg" class="img1" id="img1" alt="" srcset="">
+<img src="img/gallery/child-g8a0f00a1b_1920.jpg" class="img2" id="img2" alt="img2" srcset="">
+<img src="img/gallery/mom-g75d6802b1_1920.jpg"  class="img3" id="img3"alt="img3" srcset="">
+<img src="img/gallery/children-gd59b299cc_1920.jpg"  class="img4" id="img4"alt="img4" srcset="">
+<img src="img/gallery/child-gd09a10721_1920.jpg"  class="img5" id="img5"alt="img5" srcset="">
+<img src="img/gallery/children-g8b313f47c_1920.jpg"  class="img6" id="img6"alt="img6" srcset="">
+	</div>
+</div>
+
+<!-- End section_gallery -->
+<!-- EVENTS SECTION -->
+<div class="events_section">
+	<div class="evently">
+		<h1 class="title_events">UpComing  <span style="color: #FF7D05;">Events</span></h1>
+		<p class="upcoming_subtitle">
+		Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+		</p>
+		<div class="events1">
+			<div class="bg_calendar"></div>
+			<h2 class="day_event1">10</h2>
+			<h2 class="mouth_event1">AUG</h2>
+			<h2 class="event_title">Seance De Formation avec les femmes de sorofin</h2>
+			<img src="img/icon/clock.png" alt="" class="clock" id="clock">
+			<span class="start_time">12:00 am</span>
+			<span class="inter_time">-</span>
+			<span class="end_time">5:00 pm </span>
+			<p class="paragraph_events">
+			Lorem Ipsum is simply dummy text of the printing
+			 and typesetting industry. Lorem Ipsum has been 
+			 the industry's standard dummy text ever since the 1500s,
+			  when an unknown printer took a galley of type and scrambled
+			it to make a type specimen book. It has survived not only five
+			centuries, but also the leap into electronic typesetting, remaining
+			 essentially unchanged. It was popularised in the 1960s with the release
+			  of Letraset sheets containing Lorem Ipsum passages, and more recently 
+			  with desktop publishing software like Aldus PageMaker including versions
+			   of Lorem Ipsum.
+			</p>
+		</div>
+		<div class="events2">
+			<div class="bg_calendar2"></div>
+			<h2 class="day_event2">10</h2>
+			<h2 class="mouth_event2">AUG</h2>
+			<h2 class="event_title2">Seance De Formation avec les femmes de sorofin</h2>
+			 <img src="img/icon/clock.png" alt="" class="clock_2" id="clock">
+			<span class="start_time2">12:00 am</span>
+			<span class="inter_time2">-</span>
+			<span class="end_time2">5:00 pm </span>
+			<p class="paragraph_events2">
+			Lorem Ipsum is simply dummy text of the printing
+			 and typesetting industry. Lorem Ipsum has been 
+			 the industry's standard dummy text ever since the 1500s,
+			  when an unknown printer took a galley of type and scrambled
+			it to make a type specimen book. It has survived not only five
+			centuries, but also the leap into electronic typesetting, remaining
+			 essentially unchanged. It was popularised in the 1960s with the release
+			  of Letraset sheets containing Lorem Ipsum passages, and more recently 
+			  with desktop publishing software like Aldus PageMaker including versions
+			   of Lorem Ipsum.
+			</p> 
+		</div>
+	</div>
+</div>
+<!-- End EVENTS SECTION -->
+<!-- Partenaires Section -->
+<div class="parteners_section">
+	<h1 class="parteners_section_title">
+	Our  <span style="color: #FF7D05;">Partners</span>
+	</h1>
+	<p class="parteners_section_subtitle">
+	Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+	</p>
+	<div class="parteners_class">
+		<img src="img/parteners/" alt="" class="partener1" id="partener1" >
+		<img src="img/parteners/" alt="" class="partener2" id="partener2" >
+		<img src="img/parteners/" alt="" class="partener3" id="partener3" >
+	</div>
+	<div class="become_partener">
+		<h3 class="become_partener_text">
+		Join your hand with us  for a better life and beautiful future.
+		</h3>
+		<button class="btn_become_partener" id="btn_become_partener" ><a class="text_btn" href="#">Become Parteners</a></button>
+	</div>
+</div>
+<!-- End Partenaires Section -->
+<!-- FOOOOOOOOOOOOTER -->
+<div class="footer_section">
+	<div class="footer_transparent"></div>
+
+
+</div>
+	
+<!-- End FOOOOOOOOOOOOTER -->
+
+</body>
+</html>
